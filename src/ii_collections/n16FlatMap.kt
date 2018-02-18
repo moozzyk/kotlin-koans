3afcbb8bd31d1e1fe0053a0de1ecec23cecda7cf
moozzyk <moozzyk@gmail.com>
2018-02-18 13:21:00 -0800

    task16

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,18 +1,7 @@
 package ii_collections
 
-fun example() {
+// Return all products this customer has ordered
+val Customer.orderedProducts: Set<Product> get() = orders.flatMap { it.products }.toSet()
 
-    val result = listOf("abc", "12").flatMap { it.toList() }
-
-    result == listOf('a', 'b', 'c', '1', '2')
-}
-
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
-
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+// Return all products that were ordered by at least one customer
+val Shop.allOrderedProducts: Set<Product> get() = customers.flatMap { it.orderedProducts }.toSet()
