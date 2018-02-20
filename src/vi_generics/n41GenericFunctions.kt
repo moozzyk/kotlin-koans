798b1639c6275827948688b05571945a6f6b9524
moozzyk <moozzyk@gmail.com>
2018-02-19 23:13:50 -0800

    task41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,22 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: MutableCollection<in T>> Iterable<T>.partitionTo(c1: C, c2: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (item in this) {
+        if (predicate(item)) {
+            c1.add(item)
+        } else {
+            c2.add(item)
+        }
+    }
+
+    return Pair(c1, c2)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
